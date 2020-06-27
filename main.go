35df98410ac49fce0d45736126fdc6545e78c614
teddyking <edteddyking@gmail.com>
2020-06-27 16:57:57 +0100

    Initial bootstrap

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ed Bread Redemption")
-}
-
 func main() {
 	port := os.Getenv("PORT")
 
-	http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
